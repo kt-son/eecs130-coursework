6d2c842df83b11233ee7294188af706c5b7e4fdd
KT Son <yujung@dhcp-10-105-35-205.wireless.northwestern.private>
2019-04-26 13:41:08 -0500

    Adding my Lab04 to the repo

---
+++ lab04/font-switcher/js/index.js
@@ -0,0 +1,11 @@
+const makeBigger = () => {
+   document.querySelector('.content').style.fontSize = '36px';
+};
+
+const makeSmaller = () => {
+   document.querySelector('.content').style.fontSize = '8px';
+};
+
+
+document.querySelector('.a1').onclick = makeBigger;
+document.querySelector('.a2').onclick = makeSmaller;
