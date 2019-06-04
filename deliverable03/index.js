9c22d16b2eee5295c1512d978b1908e44183599b
KT Son <yujung@dhcp-10-105-24-253.wireless.northwestern.private>
2019-06-03 19:13:59 -0500

    Adding my deliverable03 to the repo

---
+++ deliverable03/index.js
@@ -0,0 +1,24 @@
+const search = (ev) => {
+  let term = document.querySelector('input').value;
+  let url = 'https://www.apitutor.org/yelp/simple/v3/businesses/search?limit=5&location=' + term;
+  fetch(url)
+    .then(response => response.json())
+    .then(displayResults);
+};
+
+  const displayResults = (data) => {
+    document.querySelector(".restaurants").innerHTML = ""
+    for (item of data) {
+      const template = `
+        <div class = "card">
+          <div class = "photo" style = "background-image: url('${item.image_url}')"></div>
+          <p class = "name">${item.name}</p>
+          <p class = "address">${item.display_address}</p>
+        </div>
+      `
+      console.log(template);
+      document.querySelector(".restaurants").innerHTML += template;
+    };
+  };
+
+    document.querySelector("#search").onclick = search;
